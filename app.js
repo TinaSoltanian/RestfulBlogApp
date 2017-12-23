43903a139a4a4169515c3c1a2bb75da01d2ac4af
EC2 Default User <ec2-user@ip-172-31-23-157.ec2.internal>
2017-12-23 22:33:06 +0000

    add show page to show the whole blog content and style it, change index page to just shoe first 100 chatachter of the each blog

--- app.js
+++ app.js
@@ -59,6 +59,17 @@ app.post("/blogs",function(req, res){
     })
 })
 
+app.get("/blogs/:id", function(req, res) {
+   Blog.findById(req.params.id, function(err, foundBlog){
+       if(err){
+          res.redirect("/blogs");
+       }
+       else{
+           res.render("show", {blog: foundBlog});
+       }
+   })
+});
+
 app.listen(process.env.PORT, process.env.IP, function(req, res){
     console.log("Server has started");
 });
